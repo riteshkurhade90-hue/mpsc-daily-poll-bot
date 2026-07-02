f60534b751a857c25ce196ce5232a4319a7cf7f7
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-03 03:05:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ CHAT_ID = os.environ["CHANNEL_USERNAME"]
 # Current Day
 # ===========================
 
-CURRENT_DAY = int(time.strftime("%d"))
+CURRENT_DAY = 3
 
 print(f"Today's Day : {CURRENT_DAY}")
 
