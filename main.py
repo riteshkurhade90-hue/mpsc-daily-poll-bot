f23c019eea3c15ea9a433da8d63a64d5feac4b31
riteshkurhade90-hue <riteshkurhade90@gmail.com>
2026-07-03 01:41:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,19 @@
-import gspread
-import datetime
-import requests
-import json
-import base64
 import os
-import time
+import base64
 
-# ENV
-TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
-CHANNEL_USERNAME = os.environ["CHANNEL_USERNAME"]
+print("===== BOT STARTED =====")
 
-# Decode credentials
+credentials = os.getenv("GOOGLE_CREDENTIALS")
+
+if credentials is None:
+    raise Exception("GOOGLE_CREDENTIALS Secret सापडला नाही.")
+
+print("Secret मिळाला.")
+
+decoded = base64.b64decode(credentials)
+
 with open("credentials.json", "wb") as f:
-    f.write(base64.b64decode(os.environ["GOOGLE_CREDENTIALS"]))
+    f.write(decoded)
 
-gc = gspread.service_account(filename="credentials.json")
-
-sheet = gc.open("Telegram Quiz Automation").sheet1
-
-# TODAY
-today = datetime.date.today().strftime("%Y-%m-%d")
-
-data = sheet.get_all_records()
-
-url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendPoll"
-
-# LOOP ALL 25 QUESTIONS
-for i, row in enumerate(data):
-
-    if row["Date"] == today and row["Status"] != "Posted":
-
-        try:
-            question = row["Question"]
-
-            options = [
-                row["Option 1"],
-                row["Option 2"],
-                row["Option 3"],
-                row["Option 4"]
-            ]
-
-            payload = {
-                "chat_id": CHANNEL_USERNAME,
-                "question": question,
-                "options": json.dumps(options),
-                "type": "quiz",
-                "correct_option_id": int(row["Correct Option"]) - 1,
-                "is_anonymous": False
-            }
-
-            res = requests.post(url, data=payload)
-
-            print("Posted:", question, res.status_code)
-
-            if res.status_code == 200:
-                sheet.update_cell(i + 2, 11, "Posted")
-
-            time.sleep(2)  # IMPORTANT: Telegram safe delay
-
-        except Exception as e:
-            print("Error:", e)
+print("credentials.json तयार झाली.")
+print("===== SUCCESS =====")
